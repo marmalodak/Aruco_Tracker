f89e6ad1f99d2a7bada21f72def63dac2fd54033
Johan Schmitt <johns@bacmac.local>
2021-06-30 21:47:19 -0700

    Very slow demo that finds the correct aruco marker

--- aruco_tracker.py
+++ aruco_tracker.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Framework   : OpenCV Aruco
 Description : Calibration of camera and using that for finding pose of multiple markers
@@ -13,7 +14,38 @@ import cv2
 import cv2.aruco as aruco
 import glob
 
-cap = cv2.VideoCapture(1)
+from icecream import ic
+
+
+aruco_dicts = [
+    (aruco.DICT_4X4_100,        'DICT_4X4_100'),
+    (aruco.DICT_4X4_1000,       'DICT_4X4_1000'),
+    (aruco.DICT_4X4_250,        'DICT_4X4_250'),
+    (aruco.DICT_4X4_50,         'DICT_4X4_50'),
+    (aruco.DICT_5X5_100,        'DICT_5X5_100'),
+    (aruco.DICT_5X5_1000,       'DICT_5X5_1000'),
+    (aruco.DICT_5X5_250,        'DICT_5X5_250'),
+    (aruco.DICT_5X5_50,         'DICT_5X5_50'),
+    (aruco.DICT_6X6_100,        'DICT_6X6_100'),
+    (aruco.DICT_6X6_1000,       'DICT_6X6_1000'),
+    (aruco.DICT_6X6_250,        'DICT_6X6_250'),
+    (aruco.DICT_6X6_50,         'DICT_6X6_50'),
+    (aruco.DICT_7X7_100,        'DICT_7X7_100'),
+    (aruco.DICT_7X7_1000,       'DICT_7X7_1000'),
+    (aruco.DICT_7X7_250,        'DICT_7X7_250'),
+    (aruco.DICT_7X7_50,         'DICT_7X7_50'),
+    (aruco.DICT_APRILTAG_16H5,  'DICT_APRILTAG_16H5'),
+    (aruco.DICT_APRILTAG_16h5,  'DICT_APRILTAG_16h5'),
+    (aruco.DICT_APRILTAG_25H9,  'DICT_APRILTAG_25H9'),
+    (aruco.DICT_APRILTAG_25h9,  'DICT_APRILTAG_25h9'),
+    (aruco.DICT_APRILTAG_36H10, 'DICT_APRILTAG_36H10'),
+    (aruco.DICT_APRILTAG_36H11, 'DICT_APRILTAG_36H11'),
+    (aruco.DICT_APRILTAG_36h10, 'DICT_APRILTAG_36h10'),
+    (aruco.DICT_APRILTAG_36h11, 'DICT_APRILTAG_36h11'),
+    (aruco.DICT_ARUCO_ORIGINAL, 'DICT_ARUCO_ORIGINAL'),
+]
+
+cap = cv2.VideoCapture(0)
 
 ####---------------------- CALIBRATION ---------------------------
 # termination criteria for the iterative algorithm
@@ -30,7 +62,7 @@ imgpoints = [] # 2d points in image plane.
 
 # iterating through all calibration images
 # in the folder
-images = glob.glob('calib_images/*.jpg')
+images = glob.glob('calib_images/checkerboard/*.jpg')
 
 for fname in images:
     img = cv2.imread(fname)
@@ -54,7 +86,12 @@ for fname in images:
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
+# detector parameters can be set here (List of detection parameters[3])
+parameters = aruco.DetectorParameters_create()
+parameters.adaptiveThreshConstant = 10
+
 ###------------------ ARUCO TRACKER ---------------------------
+dict_index = 0
 while (True):
     ret, frame = cap.read()
 
@@ -62,51 +99,61 @@ while (True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # set dictionary size depending on the aruco marker selected
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    #aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    
+    for aruco_dict, aruco_dict_name in aruco_dicts:
 
-    # detector parameters can be set here (List of detection parameters[3])
-    parameters = aruco.DetectorParameters_create()
-    parameters.adaptiveThreshConstant = 10
+        # lists of ids and the corners belonging to each id
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco.Dictionary_get(aruco_dict), parameters=parameters)
 
-    # lists of ids and the corners belonging to each id
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+        # font for displaying text (below)
+        font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # font for displaying text (below)
-    font = cv2.FONT_HERSHEY_SIMPLEX
+        # check if the ids list is not empty
+        # if no check is added the code will crash
+        #if np.all(ids != None):
+        if np.all(ids):
 
-    # check if the ids list is not empty
-    # if no check is added the code will crash
-    if np.all(ids != None):
+            # estimate pose of each marker and return the values
+            # rvet and tvec-different from camera coefficients
+            rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
+            #(rvec-tvec).any() # get rid of that nasty numpy value array error
 
-        # estimate pose of each marker and return the values
-        # rvet and tvec-different from camera coefficients
-        rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
-        #(rvec-tvec).any() # get rid of that nasty numpy value array error
+            for i in range(0, ids.size):
+                # draw axis for the aruco markers
+                aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
 
-        for i in range(0, ids.size):
-            # draw axis for the aruco markers
-            aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
-
-        # draw a square around the markers
-        aruco.drawDetectedMarkers(frame, corners)
-
-
-        # code to show ids of the marker found
-        strg = ''
-        for i in range(0, ids.size):
-            strg += str(ids[i][0])+', '
-
-        cv2.putText(frame, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+            # draw a square around the markers
+            aruco.drawDetectedMarkers(frame, corners)
 
 
-    else:
-        # code to show 'No Ids' when no markers are found
-        cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+            # code to show ids of the marker found
+            strg = ''
+            for i in range(0, ids.size):
+                strg += str(ids[i][0])+', '
 
-    # display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            # cv2.putText(frame, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+            cv2.putText(frame, f'{aruco_dict_name}: Id: {strg}', (0, 64), font, 1, (0,255,0), 2, cv2.LINE_AA)
+
+
+        else:
+            # code to show 'No Ids' when no markers are found
+            cv2.putText(frame, "No Ids", (0,256), font, 1, (0,255,0),2,cv2.LINE_AA)
+
+        rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(rejectedImgPoints, 0.05, mtx, dist)
+
+        if rejectedImgPoints:
+            cv2.putText(frame, "Rejected", (0,128), font, 1, (0,255,0),2,cv2.LINE_AA)
+
+            for i,p in enumerate(rejectedImgPoints):
+                aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
+
+        aruco.drawDetectedMarkers(frame, rejectedImgPoints)
+
+        # display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 # When everything done, release the capture
 cap.release()
